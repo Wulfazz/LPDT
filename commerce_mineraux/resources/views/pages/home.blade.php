a42e39ada354c757aaf1b2698e1a46c09e0cbc88
Wulfazz <teito.klein@laposte.net>
2024-04-23 16:14:25 +0200

    update home tel

--- commerce_mineraux/resources/views/pages/home.blade.php
+++ commerce_mineraux/resources/views/pages/home.blade.php
@@ -7,15 +7,82 @@
             <header>@include('components.header')</header>
 
             <main>
+                <!-- Section intro -->
                 <section class="intro">
-                    <div class="overlay"></div>
-                    <H2>LES PIERRES DE LA TERRE</H2>
+                    <div>
+                        <img src="{{ asset('img/home/LPDT-home.jpg') }}" alt="">
+                    </div>
                     <p>
                         La passion des minéraux, vente de minéraux et bijoux.
                         <br>
                         Ici, vous allez découvrir une collection sélectionnée par 
                         une passionnée de minéraux.
                     </p>
+                </section>
+
+                <!-- Section catalogue -->
+                <section class="catalog">
+                    <h2>NOTRE CATALOGUE DE MINERAUX</h2>
+
+                    <div class="cat">
+                        <img src="{{ asset('img/home/brute.jpg') }}" alt="">
+                        <button>
+                            <p>Minéraux brutes</p>
+                        </button>
+                    </div>
+
+                    <div class="cat">
+                        <img src="{{ asset('img/home/pierres.jpg') }}" alt="">
+                        <button>
+                            <p>Pierres taillées</p>
+                        </button>
+                    </div>
+
+                    <div class="cat">
+                        <img src="{{ asset('img/home/bracelets.jpg') }}" alt="">
+                        <button>
+                            <p>Bracelets</p>
+                        </button>
+                    </div>
+
+                    <div class="cat">
+                        <img src="{{ asset('img/home/pendentifs.jpg') }}" alt="">
+                        <button>
+                            <p>Pendentifs</p>
+                        </button>
+                    </div>
+                </section >
+
+                <!-- Section verifs / paiements -->
+                <section class="icons">
+                    <div class="verifs">
+                        <div>
+                            <img src="" alt="">
+                            <p>100% naturelles</p>
+                        </div>
+                        <div>
+                            <img src="" alt="">
+                            <p>Paiements sécurisés</p>
+                        </div>
+                        <div>
+                            <img src="" alt="">
+                            <p>Retours et livraison soignée</p>
+                        </div>
+                    </div>
+                    <div class="payments">
+                        <div>
+                            <img src="" alt="">
+                            <p>Paiement par chèque</p>
+                        </div>
+                        <div>
+                            <img src="" alt="">
+                            <p>Paiement Paypal</p>
+                        </div>
+                        <div>
+                            <img src="" alt="">
+                            <p>Paiement par virement</p>
+                        </div>
+                    </div>
                 </section>
             </main>
         
