59b361a9137bc2cd15bb0fedd35557ed7eedc694
Wulfazz <teito.klein@laposte.net>
2024-06-09 16:42:16 +0200

    finish

--- commerce_mineraux/resources/views/components/menuhidden.blade.php
+++ commerce_mineraux/resources/views/components/menuhidden.blade.php
@@ -1,36 +1,36 @@
 <div class="menu-container">
     <div class="menu-hidden" id="mobileMenu">
-        <a href="{{ url('/') }}">Accueil</a>
-        <a href="{{ url('/story') }}">Mon histoire</a>
-        <a href="#footer" id="linkToFooter">À propos</a>
+        <a href="{{ url('/') }}" class="menu-link">Accueil</a>
+        <a href="{{ url('/story') }}" class="menu-link">Mon histoire</a>
+        <a href="#footer" id="linkToFooter" class="menu-link">À propos</a>
         <div class="dropdown">
-            <a href="javascript:void(0);" class="dropbtn">Boutique</a>
+            <a href="javascript:void(0);" class="dropbtn menu-link">Boutique</a>
             <div class="dropdown-content">
-                <a href="{{ url('/shop/minerals') }}">Minéraux bruts</a>
-                <a href="{{ url('/shop/stone') }}">Pierres taillées</a>
-                <a href="{{ url('/shop/bracelet') }}">Bracelets</a>
-                <a href="{{ url('/shop/pendant') }}">Pendentifs</a>
+                <a href="{{ url('/shop/minerals') }}" class="dropdown-link">Minéraux bruts</a>
+                <a href="{{ url('/shop/stone') }}" class="dropdown-link">Pierres taillées</a>
+                <a href="{{ url('/shop/bracelet') }}" class="dropdown-link">Bracelets</a>
+                <a href="{{ url('/shop/pendant') }}" class="dropdown-link">Pendentifs</a>
             </div>
         </div>
-        <a href="{{ url('/contact') }}">Contact</a>
+        <a href="{{ url('/contact') }}" class="menu-link">Contact</a>
 
         @guest
-            <a href="{{ url('/login') }}">Connexion</a>
-            <a href="{{ url('/register') }}">Inscription</a>
+            <a href="{{ url('/login') }}" class="menu-link">Connexion</a>
+            <a href="{{ url('/register') }}" class="menu-link">Inscription</a>
         @else
-            <a href="{{ url('/profile/' . Auth::user()->user_id) }}">Profil</a>
+            <a href="{{ url('/profile/' . Auth::user()->user_id) }}" class="menu-link">Profil</a>
             @if(Auth::user()->user_type === 'seller')
                 <div class="dropdown">
-                    <a href="javascript:void(0);" class="dropbtn">Admin</a>
+                    <a href="javascript:void(0);" class="dropbtn menu-link">Admin</a>
                     <div class="dropdown-content">
-                        <a href="{{ url('/admin/users') }}">Gérer Utilisateurs</a>
-                        <a href="{{ url('/admin/products') }}">Gérer Produits</a>
-                        <a href="{{ url('/admin/products/create') }}">Ajouter Produit / Gérer Catégories</a>
+                        <a href="{{ url('/admin/users') }}" class="dropdown-link">Gérer Utilisateurs</a>
+                        <a href="{{ url('/admin/products') }}" class="dropdown-link">Gérer Produits</a>
+                        <a href="{{ url('/admin/products/create') }}" class="dropdown-link">Ajouter Produit / Gérer Catégories</a>
                     </div>
                 </div>
             @endif
             <a href="{{ url('/logout') }}"
-               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+               onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="menu-link">
                 Déconnexion
             </a>
             <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
